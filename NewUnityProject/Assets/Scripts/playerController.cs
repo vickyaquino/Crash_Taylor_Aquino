cad6edc796e9d0fde185fba10f5322813a657015
vickyaquino <vickyaquino1989@gmail.com>
2023-10-26 14:38:54 -0700

    fixing eye on player, put some code in player controller

--- NewUnityProject/Assets/Scripts/playerController.cs
+++ NewUnityProject/Assets/Scripts/playerController.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    //amount of lives main player has in the beginning of the game.
+    public int Lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
