58a3898b6accba5cbed2f68be33ff86c94e5cfc5
vickyaquino <vickyaquino1989@gmail.com>
2023-10-26 19:13:19 -0700

    Fixed the platform thingy
    Added two more points for front and back and made a second move thingy for the platform.

--- NewUnityProject/Assets/Scripts/UI.cs
+++ NewUnityProject/Assets/Scripts/UI.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject MainMenu, PauseMenu, GameOver;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public PlayerController PlayerScript;
+
+    bool IsPaused;
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        IsPaused = false;
+        //come back to put in sceneloader 'loadscene' as a function
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Pause()
     {
-        
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        IsPaused = true;
+        PauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        IsPaused = false;
+        PauseMenu.SetActive(false);
+    }
+    private void Update()
+    {
+        if (!IsPaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+        else if (IsPaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Resume();
+        }
+        if (PlayerScript.Lives <= 0) gameOver();
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void gameOver()
+    {
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        GameOver.SetActive(true);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- NewUnityProject/Assets/Scripts/platform.cs
+++ NewUnityProject/Assets/Scripts/platform.cs
@@ -11,22 +11,32 @@ public class platform : MonoBehaviour
     //game objects to determine how far left and right the enemy goes
     public GameObject LeftPoint;
     public GameObject RightPoint;
+    public GameObject ForwardPoint;
+    public GameObject BackmostPoint;
 
     //boundary points for left and right
     private Vector3 LeftPos;
     private Vector3 RightPos;
+    private Vector3 FrontPos;
+    private Vector3 BackPos;
 
     //how fast enemy travels
     public float speed;
 
     //the direction enemy travels 
     public bool GoingLeft;
+    public bool GoingBackward;
+
+    [Header("Move Forward or Left?")]
+    public bool MoveLeftRight;
 
     // Start is called before the first frame update
     void Start()
     {
         LeftPos = LeftPoint.transform.position;
         RightPos = RightPoint.transform.position;
+        FrontPos = ForwardPoint.transform.position;
+        BackPos = BackmostPoint.transform.position;
     }
 
     // Update is called once per frame
@@ -38,29 +48,60 @@ public class platform : MonoBehaviour
     //makes the enemy move left and right
     private void PlatformMove()
     {
-        if (GoingLeft)
+        if (MoveLeftRight)
         {
-            //once enemy reaches left point left turns false
-            if (transform.position.x <= LeftPos.x)
+            if (GoingLeft)
             {
-                GoingLeft = false;
-            }
+                //once enemy reaches left point left turns false
+                if (transform.position.x <= LeftPos.x)
+                {
+                    GoingLeft = false;
+                }
 
+                else
+                {
+                    transform.position += Vector3.left * speed * Time.deltaTime;
+                }
+            }
             else
             {
-                transform.position += Vector3.left * speed * Time.deltaTime;
+                //once the enemy reaches RightPosition, GoingLeft is true
+                if (transform.position.x >= RightPos.x)
+                {
+                    GoingLeft = true;
+                }
+                else
+                {
+                    transform.position += Vector3.right * speed * Time.deltaTime;
+                }
             }
         }
-        else
+        else if (!MoveLeftRight)
         {
-            //once the enemy reaches RightPosition, GoingLeft is true
-            if (transform.position.x >= RightPos.x)
+            if (GoingBackward)
             {
-                GoingLeft = true;
+                //once enemy reaches left point left turns false
+                if (transform.position.z <= FrontPos.z)
+                {
+                    GoingBackward = false;
+                }
+
+                else
+                {
+                    transform.position += Vector3.back * speed * Time.deltaTime;
+                }
             }
             else
             {
-                transform.position += Vector3.right * speed * Time.deltaTime;
+                //once the enemy reaches RightPosition, GoingLeft is true
+                if (transform.position.z >= BackPos.z)
+                {
+                    GoingBackward = true;
+                }
+                else
+                {
+                    transform.position += Vector3.forward * speed * Time.deltaTime;
+                }
             }
         }
     }
